df302e5fd6c72edc152bcf57d00ebebc98f5d34e
Rob Clark <robdclark@gmail.com>
2017-08-11 08:58:50 -0400

    travis-ci: updates to run OpenBSD's efi bootloader
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- py/travis-ci/u_boot_boardenv_vexpress_ca15_tc2_qemu.py
+++ py/travis-ci/u_boot_boardenv_vexpress_ca15_tc2_qemu.py
@@ -5,3 +5,4 @@ env__net_dhcp_server = True
 env__net_tftp_readable_file = travis_tftp.file2env('u-boot')
 env__efi_loader_helloworld_file = travis_tftp.file2env('lib/efi_loader/helloworld.efi')
 env__efi_loader_grub_file = travis_tftp.file2env('grub_arm.efi')
+env__efi_loader_openbsd_file = travis_tftp.file2env('BOOTARM.EFI')
